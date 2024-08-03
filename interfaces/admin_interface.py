6ee77920c5e4ebf540eb083460a6964b5e89fe81
joe-kranio <jonathan@kranio.io>
2024-08-03 00:09:08 -0500

    add admin panel

--- interfaces/admin_interface.py
+++ interfaces/admin_interface.py
@@ -10,13 +10,12 @@ from domain.entities.violation import Violation
 app = Flask(__name__)
 init_db(app)
 
-admin = Admin(app, name='Admin', template_mode='bootstrap3')
+admin = Admin(app, name='Traffic Infraction Registration System Admin', template_mode='bootstrap3')
 
-# Add views for the models
+# Add views for each model
 admin.add_view(ModelView(Person, db.session))
 admin.add_view(ModelView(Vehicle, db.session))
 admin.add_view(ModelView(Officer, db.session))
-admin.add_view(ModelView(Violation, db.session))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- interfaces/api_interface.py
+++ interfaces/api_interface.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from infrastructure.config.db_config import init_db
+from infrastructure.config.db_config import init_db, db
 from infrastructure.flask.controllers.violation_controller import violation_bp
 from infrastructure.flask.controllers.person_controller import person_bp
 from infrastructure.flask.controllers.vehicle_controller import vehicle_bp
@@ -18,6 +18,7 @@ app.register_blueprint(officer_bp, url_prefix='/api')
 app.register_blueprint(violation_bp, url_prefix='/api')
 app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(report_bp, url_prefix='/api')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
